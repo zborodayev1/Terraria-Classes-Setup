46ff40f464fc331c8043ead5ac43a97b1745cb32
ZakharB14 <s96900393@gmail.com>
2024-09-26 14:08:02 +0500

    upd error "page not found"
    2

--- src/assets/Errors/ErrorNotFound.jsx
+++ src/assets/Errors/ErrorNotFound.jsx
@@ -1,7 +1,8 @@
+import styles from '../../components/css/App.module.scss'
 export const ErrorNotFound = () => {
   return (
-    <div>
-        <h1 className="text-3xl flex justify-center mt-3" >Page not found</h1>
+    <div className={styles.main}>
+        <h1 className="text-3xl flex justify-center mt-3">Page not found</h1>
     </div>
   )
 }
